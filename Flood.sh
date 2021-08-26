b410e9ecc95dd6f5c03f3cfcdf17814b5ca04ec2
Wemis <88230804+Wemis@users.noreply.github.com>
2021-08-26 12:00:08 +0300

    Update Flood.sh

--- Flood.sh
+++ Flood.sh
@@ -3,6 +3,7 @@ import time
 from aiogram import *
 
 a=input("Введите токен") 
+s=input("Text message") 
 bot = Bot(a)
 dp = Dispatcher(bot)
 
@@ -10,7 +11,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=["flood"])
 async def flood(message: types.Message):
     try:
-        message_to_forward = await message.answer("Text message")
+        message_to_forward = await message.answer(s)
         for i in range(int(message.text.replace("/flood ", ""))):
             await message_to_forward.forward(message.chat.id, False)
     except:
