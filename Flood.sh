d2d37b779a83c8ac4d248e31bd511421f8593437
Wemis <88230804+Wemis@users.noreply.github.com>
2021-08-26 11:49:26 +0300

    Create Flood.sh

---
+++ Flood.sh
@@ -0,0 +1,22 @@
+import time
+
+from aiogram import *
+
+a=input("Введите токен") 
+bot = Bot(a)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=["flood"])
+async def flood(message: types.Message):
+    try:
+        message_to_forward = await message.answer("Text message")
+        for i in range(int(message.text.replace("/flood ", ""))):
+            await message_to_forward.forward(message.chat.id, False)
+    except:
+        await message.reply("Error")
+        time.sleep(10)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
